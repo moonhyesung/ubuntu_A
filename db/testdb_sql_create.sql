dfea266d15caf4093276a5d979594c87e410c96f
moonhyesung <wldnjswjdqhr@naver.com>
2022-11-16 12:04:43 +0900

    Update testdb_sql_create.sql

--- db/testdb_sql_create.sql
+++ db/testdb_sql_create.sql
@@ -1,11 +1,10 @@
 create database testdb;
 
 show databases;
-
 use testdb;
 show tables;
 
-create table st_info(ST_ID int, NAME varchar(20), DEPT varchar(25)) default charset=utf8;
+create table st_info(ST_ID int, NAME varchar(20), DEPT varchar(25));
 show tables;
 
 create table st_grade(ST_ID int, Linux int, DB int);
@@ -20,12 +19,13 @@ alter table st_grade add constraint pk_stgrade primary key(ST_ID);
 explain st_info;
 explain st_grade;
 
-insert into st_info values(202201, "LeeGilDong", "Game");
-insert into st_info values(202202, "KimGilDong", "Computer");
-insert into st_info values(202203,"HongGilDong", "Game");
+insert into st_info values(202201, 'LeeGilDong', 'Game');
+insert into st_info values(202202, 'KimGilDong', 'Computer');
+insert into st_info values(202203, 'HongGilDong', 'Game'); 
 select * from st_info;
 
-insert into st_grade values(202201, 80 ,70);
+insert into st_grade values(202201, 80, 70);
 insert into st_grade values(202202, 90, 75);
-insert into st_grade values(202203, 95, 85);
-select * from st_grade
+insert into st_grade values(202203, 95, 85); 
+select * from st_grade;
+
